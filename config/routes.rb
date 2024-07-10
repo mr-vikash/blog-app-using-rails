74502c47949725de72a09d57814c3ccdd15dbdbb
mr-vikash <mrvikash700@gmail.com>
2024-07-10 14:56:32 +0530

    learning resource routing and article_path helper method

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "articles#index"
-  get "/articles" , to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+
+  resources :articles
   # Defines the root path route ("/")
   # root "posts#index"
 end
